269a6b5ce908fb0fcbbdc3dcf83a9657a656e899
carlitheo1 <carli.theodore@yahoo.com>
2023-10-26 11:04:30 -0500

    remade navigation bar
    -got hamburger menu to be responsive and appear when screen size changes
    -changed old bar to fit width: 100%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // JS scripts placed here
+function myFunction() {
+    var x = document.getElementById("brokenav");
+    if (x.className === "broke-bear-header-bar") {
+      x.className += " responsive";
+    } else {
+      x.className = "broke-bear-header-bar";
+    }
+  }
